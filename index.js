302a98909e38ee6892bdcc99ed6727c89ace43d6
Kou <kimura@kurashicom.jp>
2018-07-28 22:29:32 +0900

    Input control

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message1: 'Hello Vue!'
     }
 });
 
@@ -10,7 +10,7 @@ var app2 = new Vue({
   data: {
     seen: true
   }
-})
+});
 
 var app4 = new Vue({
     el: '#app-4',
@@ -20,5 +20,24 @@ var app4 = new Vue({
             {text: 'Learn Vue'},
             {text: 'Build something awesome'}
         ]
+    }
+});
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message2: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message2 = this.message2.split('').reverse().join('')
+        }
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message3: 'Hello Vue.js!'
     }
 })
